d0bee7cae835aa9d314f5d6ef3503b0004b5c016
Erisadwi <erisadwi099@gmail.com>
2026-06-13 11:27:38 +0700

    modifikasi panduan

--- landing_page.php
+++ landing_page.php
@@ -316,9 +316,10 @@ body{
             </a>
         </li>
             <li>Login to your account to get your API Key.</li>
-            <li>Copy your API Key from Dashboard.</li>
-            <li>Add API Key in request header.</li>
-            <li>Send request to endpoint.</li>
+            <li>If you are using Postman, copy your API Key from the Dashboard and add it to the request header.</li>
+            <li>If you are using the Web Client, simply log in to your account. The API Key will be sent automatically by the system./li>
+            <li>Send requests to the available endpoints.</li>
+            <li>View and manage the returned data directly through the website.</li>
         </ol>
 
     </div>
